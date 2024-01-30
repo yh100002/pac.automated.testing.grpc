19f545e39a61be905766495dfd310cfa56e963f0
YounghoSon <yh100002@gmail.com>
2024-01-30 17:31:04 +0100

    to trigger deploy to AWS ECR

--- Program.cs
+++ Program.cs
@@ -19,9 +19,9 @@ builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 app.MapGrpcService<PacPlatformTeamMembersService>();
-//app.MapCodeFirstGrpcReflectionService();
+
 app.MapGrpcReflectionService();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
